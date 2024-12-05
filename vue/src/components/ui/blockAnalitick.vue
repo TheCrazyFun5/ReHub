fb39a3ffdc5225c8a3926b4085b3423d9d7357b5
HACYYY <kkllovn@gmail.com>
2024-12-05 22:14:42 +0500

    PageShift (25%)

---
+++ vue/src/components/ui/blockAnalitick.vue
@@ -0,0 +1,71 @@
+<script>
+export default {
+  props: {
+    textChange: {
+      type: String,
+      Required: true,
+    },
+    maininfoChange: {
+      type: String,
+      Required: true,
+    },
+    discriptChange: {
+      type: String,
+      Required: true,
+    },
+
+    imgChange: {
+      type: String,
+      Required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="mainBlock">
+    <header>
+      <div class="nameTag">{{ textChange }}</div>
+      <img :src="imgChange" alt="My img" />
+    </header>
+    <div class="mainInfocard">{{ maininfoChange }}</div>
+    <div class="discriptionsCard">{{ discriptChange }}</div>
+  </div>
+</template>
+
+<style scoped>
+.mainBlock {
+  max-width: 400px;
+  max-height: 246px;
+  width: 100%;
+  height: 100%;
+  margin: 9px;
+  background-color: #ffffff;
+  padding: 30px 30px 30px 38px;
+  border-radius: 10px;
+  -webkit-box-shadow: 0px 0px 8px 2px rgba(124, 124, 124, 0.2);
+  -moz-box-shadow: 0px 0px 8px 2px rgba(124, 124, 124, 0.2);
+  box-shadow: 0px 0px 8px 2px rgba(124, 124, 124, 0.2);
+}
+header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.nameTag {
+  font-size: 40px;
+  color: #1c1c1c;
+  margin-bottom: 18px;
+}
+
+.discriptionsCard {
+  font-size: 25px;
+  color: #7c7c7c;
+  margin-top: 10px;
+}
+
+.mainInfocard {
+  font-size: 90px;
+  color: #1c1c1c;
+}
+</style>
--- vue/src/pages/shift.vue
+++ vue/src/pages/shift.vue
@@ -1,14 +1,33 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+<script>
+import image from "@/assets/img/calendar.svg";
+import clock from "@/assets/img/clock.svg";
+import blockAnalitick from "@/components/ui/blockAnalitick.vue";
+export default {
+  components: { blockAnalitick },
+  data() {
+    return {
+      image,
+      clock,
+    };
+  },
+};
 </script>
 
 <template>
-  <h1>shift</h1>
+  <div class="firstBlock">
+    <blockAnalitick textChange="Всего смен" maininfoChange="10" discriptChange="В этом месяце: 2" :imgChange="image" />
+    <blockAnalitick
+      :imgChange="clock"
+      textChange="Часов за месяц"
+      maininfoChange="72"
+      discriptChange="В этом месяце: 10"
+    />
+  </div>
 </template>
 
 <style scoped>
-h1 {
-  font-size: 30px;
-  color: green;
+.firstBlock {
+  display: flex;
+  gap: 30px;
 }
 </style>
